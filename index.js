cc9730bb3cef2d98c272b9724d6f36e64e92a681
Moringa School <james.koipapi@student.moringaschool.com>
2022-09-22 17:11:13 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'James';
+
+const welcomeMessage = `learWelcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, 'JAMES!'${currentUser}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!` ;
